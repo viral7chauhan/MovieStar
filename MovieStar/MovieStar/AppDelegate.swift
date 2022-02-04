a1c2213ce84a07856a89016f76246385abfd13c4
viral7chauhan <viral7chauhan@yahoo.com>
2022-02-04 20:00:08 +0530

    Move `MainTabBarViewController` in its own file

--- MovieStar/MovieStar/AppDelegate.swift
+++ MovieStar/MovieStar/AppDelegate.swift
@@ -28,21 +28,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return MainTabBarController()
     }
 }
-
-
-class MainTabBarController: UITabBarController {
-    convenience init() {
-        self.init(nibName: nil, bundle: nil)
-        self.setupViewControllers()
-    }
-
-    private func setupViewControllers() {
-        let topMoviesViewController = ListViewController()
-        topMoviesViewController.tabBarItem = UITabBarItem(title: "Top", image: #imageLiteral(resourceName: "star_outline"), selectedImage: #imageLiteral(resourceName: "star_filled"))
-        let myMoviesViewController = UIViewController()
-        myMoviesViewController.tabBarItem = UITabBarItem(title: "My Movies", image: #imageLiteral(resourceName: "movie_outline"), selectedImage: #imageLiteral(resourceName: "movie_filled"))
-
-        viewControllers = [topMoviesViewController, myMoviesViewController]
-        view.backgroundColor = .systemBackground
-    }
-}
---
+++ MovieStar/MovieStar/MainTabBarViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MainTabBarViewController.swift
+//  MovieStar
+//
+//  Created by Viral on 04/02/22.
+//  Copyright © 2022 View The Space. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    convenience init() {
+        self.init(nibName: nil, bundle: nil)
+        self.setupViewControllers()
+    }
+
+    private func setupViewControllers() {
+        let topMoviesViewController = ListViewController()
+        topMoviesViewController.tabBarItem = UITabBarItem(title: "Top", image: #imageLiteral(resourceName: "star_outline"), selectedImage: #imageLiteral(resourceName: "star_filled"))
+        let myMoviesViewController = UIViewController()
+        myMoviesViewController.tabBarItem = UITabBarItem(title: "My Movies", image: #imageLiteral(resourceName: "movie_outline"), selectedImage: #imageLiteral(resourceName: "movie_filled"))
+
+        viewControllers = [topMoviesViewController, myMoviesViewController]
+        view.backgroundColor = .systemBackground
+    }
+}
