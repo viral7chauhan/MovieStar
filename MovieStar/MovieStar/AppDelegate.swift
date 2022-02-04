b8313f694baba26f5b3d6f2319a8d91f49bbe2aa
viral7chauhan <viral7chauhan@yahoo.com>
2022-02-04 19:58:15 +0530

    Make `MainTabBarViewController` and setup view controller inside this class

--- MovieStar/MovieStar/AppDelegate.swift
+++ MovieStar/MovieStar/AppDelegate.swift
@@ -16,18 +16,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     private func displayTabBarViewController() {
-        let topMoviesViewController = UIViewController()
-        topMoviesViewController.tabBarItem = UITabBarItem(title: "Top", image: #imageLiteral(resourceName: "star_outline"), selectedImage: #imageLiteral(resourceName: "star_filled"))
-        let myMoviesViewController = UIViewController()
-        myMoviesViewController.tabBarItem = UITabBarItem(title: "My Movies", image: #imageLiteral(resourceName: "movie_outline"), selectedImage: #imageLiteral(resourceName: "movie_filled"))
-        let tabController = UITabBarController()
-        tabController.viewControllers = [topMoviesViewController, myMoviesViewController]
-        tabController.view.backgroundColor = .systemBackground
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = tabController
+        window?.rootViewController = rootViewController()
         window?.makeKeyAndVisible()
         MovieManager().fetchMovies { movies in
             print(movies)
         }
+    }
+
+    private func rootViewController() -> MainTabBarController {
+        return MainTabBarController()
+    }
+}
+
+
+class MainTabBarController: UITabBarController {
+    convenience init() {
+        self.init(nibName: nil, bundle: nil)
+        self.setupViewControllers()
+    }
+
+    private func setupViewControllers() {
+        let topMoviesViewController = ListViewController()
+        topMoviesViewController.tabBarItem = UITabBarItem(title: "Top", image: #imageLiteral(resourceName: "star_outline"), selectedImage: #imageLiteral(resourceName: "star_filled"))
+        let myMoviesViewController = UIViewController()
+        myMoviesViewController.tabBarItem = UITabBarItem(title: "My Movies", image: #imageLiteral(resourceName: "movie_outline"), selectedImage: #imageLiteral(resourceName: "movie_filled"))
+
+        viewControllers = [topMoviesViewController, myMoviesViewController]
+        view.backgroundColor = .systemBackground
     }
 }
---
+++ MovieStar/MovieStar/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  MovieStar
+//
+//  Created by Viral on 04/02/22.
+//  Copyright © 2022 View The Space. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
